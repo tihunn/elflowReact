1fa0d6690cb50c5845b6990606d68ccf85d704f1
tihunn <74502721+tihunn@users.noreply.github.com>
2023-03-02 16:47:36 +0300

    added cors

--- server.js
+++ server.js
@@ -1,14 +1,16 @@
 const express = require("express")
 const path = require("path")
+const cors = require("cors")
 
 const PORT = process.env.REACT_APP_PORT || 80
 
 const app = express()
+app.use(cors())
 app.use(express.static(__dirname))
 app.use(express.static(path.resolve(__dirname, "build")))
 
 app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "build", "allFLowers.html"))
+    res.sendFile(path.join(__dirname, "build", "index.html"))
 })
 
 app.listen(PORT)
