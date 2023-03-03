8cf649236c30bfdcb7bdc325f5b9fbc8228b4429
tihunn <74502721+tihunn@users.noreply.github.com>
2023-03-03 17:04:47 +0300

    i scored the variable url. I dont understend where lost port:3007, the console says it sees to REACT_APP_API_URL

--- src/http/axios.js
+++ src/http/axios.js
@@ -1,11 +1,11 @@
 import axios from "axios";
 
 const $host = axios.create({
-    baseURL: process.env.REACT_APP_API_URL
+    baseURL: "http://45.67.59.147:3007/"
 })
 
 const $authHost = axios.create({
-    baseURL: process.env.REACT_APP_API_URL
+    baseURL: "http://45.67.59.147:3007/"
 })
 
 const authInterceptor = config => {
