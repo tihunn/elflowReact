778cf2ac216cffd4140eb525c02de79337ee1005
tihunn <74502721+tihunn@users.noreply.github.com>
2023-03-27 09:22:25 +0300

    fix compressed img

--- src/Components/Carousel/Carousel.jsx
+++ src/Components/Carousel/Carousel.jsx
@@ -1,8 +1,13 @@
 import React from 'react';
-import css from "../../style/FlowerItem.module.css";
 import {Carousel} from "react-bootstrap";
 
 const CarouselComponent = (props) => {
+    let showIsCompressed = (img) => {
+        return props.compressed
+            ? process.env.REACT_APP_API_URL + "compressed/" + img
+            : process.env.REACT_APP_API_URL + img
+    }
+
     const carousel = () => {
         return <Carousel>
             { props.image.map(img => {
@@ -10,9 +15,7 @@ const CarouselComponent = (props) => {
                          <img className={props.css.img}
                               alt="картинка цветка"
                               onClick={props.onClick}
-                              src={props.compressed
-                                  ? process.env.REACT_APP_API_URL + "compressed/" + img
-                                  : process.env.REACT_APP_API_URL + img}
+                              src={ showIsCompressed(img) }
                          />
                 </Carousel.Item>
             }) }
@@ -25,7 +28,7 @@ const CarouselComponent = (props) => {
                 {props.image.length !== 1
                     ? carousel()
                     : <img className={props.css.img}
-                           src={process.env.REACT_APP_API_URL + props.image[0]}
+                           src={ showIsCompressed(props.image[0]) }
                            alt="картинка цветка"
                            onClick={props.onClick}
                     />}
